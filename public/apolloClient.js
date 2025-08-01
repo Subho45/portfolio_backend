19cf73474c0a7539dcca2428f0fae99fd922ef73
Subho45 <subhitghosh45@gmail.com>
2025-08-01 20:13:50 +0530

    Delete public directory

--- public/apolloClient.js
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-
-const client = new ApolloClient({
-  uri: 'https://your-backend.onrender.com/graphql', // 🔁 Replace with your Render backend URL
-  cache: new InMemoryCache(),
-});
-
-export default client;
