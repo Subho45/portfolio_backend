9bd75af35900502da2ecbdbe3384c47f67f94410
Subho45 <subhitghosh45@gmail.com>
2025-08-02 13:45:21 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,16 +1,14 @@
-const mysql = require('mysql');
-require('dotenv').config();
+const { Pool } = require('pg');
 
-const db = mysql.createConnection({
-  host: process.env.dpg-d26a7be3jp1c73cquqfg-a,
-  user: process.env.portfolio_o5sn_user,
-  password: process.env.bR8Qz4GNIaXcHPg3HHDG8Rx77xMNldGf,
-  database: process.env.portfolio_o5sn,
+const pool = new Pool({
+  connectionString: 'postgresql://portfolio_o5sn_user:bR8Qz4GNIaXcHPg3HHDG8Rx77xMNldGf@dpg-d26a7be3jp1c73cquqfg-a.oregon-postgres.render.com/portfolio_o5sn',
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-db.connect((err) => {
-  if (err) throw err;
-  console.log('MySQL connected!');
-});
+pool.connect()
+  .then(() => console.log('PostgreSQL connected!'))
+  .catch((err) => console.error('Connection error', err.stack));
 
-module.exports = db;
+module.exports = pool;
