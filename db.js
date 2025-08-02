8f7d5b60952cb41b5687387048eb68d004f79134
Subho45 <subhitghosh45@gmail.com>
2025-08-02 14:05:38 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,14 +1,16 @@
+require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: 'postgresql://portfolio_o5sn_user:bR8Qz4GNIaXcHPg3HHDG8Rx77xMNldGf@dpg-d26a7be3jp1c73cquqfg-a.oregon-postgres.render.com/portfolio_o5sn',
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
 });
 
 pool.connect()
-  .then(() => console.log('PostgreSQL connected!'))
-  .catch((err) => console.error('Connection error', err.stack));
+  .then(() => console.log('✅ PostgreSQL connected!'))
+  .catch((err) => console.error('❌ Connection error:', err.stack));
 
 module.exports = pool;
+
