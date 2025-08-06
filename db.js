a5d68792e453f5e98bcfafd70f0363ac2d068b46
Subho45 <subhitghosh45@gmail.com>
2025-08-06 21:54:51 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
-const mysql = require('mysql2'); // using mysql2 for better support
+const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host: process.env.MYSQLHOST,
-  port: process.env.MYSQLPORT,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 connection.connect((err) => {
@@ -18,3 +18,4 @@ connection.connect((err) => {
 });
 
 module.exports = connection;
+
